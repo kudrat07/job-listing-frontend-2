e0c7585b75167017bb4b8df8b6fcbf4dc4d2f9bb
kudrat07 <84278496+kudrat07@users.noreply.github.com>
2024-11-21 12:59:52 +0530

    Update LoginPage.js

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -5,7 +5,7 @@ export default function LoginPage() {
     return (
         <div style={{ display: "flex" }}>
             <Login />
-            <img src="" alt="" style={{ maxHeight: "100vh", width: "50vw" }} />
+            <img src="https://res.cloudinary.com/duicg4gyd/image/upload/v1732074899/signup-img_x4kzcj.jpg" alt="" style={{ maxHeight: "100vh", width: "50vw" }} />
         </div>
     );
 }
